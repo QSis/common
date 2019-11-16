13ef8f3bdb2b0591343fc08e6b2e7bf1a67f8f33
songqian6 <songqian6@jd.com>
2019-11-16 18:24:38 +0800

    error function

---
+++ errs/errs.go
@@ -0,0 +1,27 @@
+package errs
+
+import (
+	"errors"
+	"github.com/cihub/seelog"
+)
+
+func ReturnIfErrNotNil(err) {
+	if err != nil {
+		return
+	}
+}
+
+func LogIfErrNotNil(err, msg ...string) {
+	if err != nil {
+		msgs := []string{err.Error()}
+		msgs = append(msgs, msg...)
+		seelog.Error(msgs)
+	}
+}
+
+func ErrToString(err) string {
+	if err != nil {
+		return errors.New(err)
+	}
+	return ""
+}
